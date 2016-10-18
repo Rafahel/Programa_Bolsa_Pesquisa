c25585be73bb91a921cfac1b306552ed418e5a6f
Rafahel <rafahelmello@gmail.com>
2016-10-18 11:59:56 -0200

    Adicionado backgroud e mensagens de erros. Correção de bugs.

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ import matplotlib.pyplot as plt
 import Interface
 from Interface import _fromUtf8
 from PyQt4 import QtCore, QtGui
+from PyQt4.QtGui import *
 import sys
 
 
-
 class MainUIClass(QtGui.QMainWindow, Interface.Ui_MainWindow):
-
-    def __init__(self, parent = None):
+    def __init__(self, parent=None):
         super(MainUIClass, self).__init__(parent)
         self.setupUi(self)
         self.quantidadeEntradas = 1
@@ -34,7 +33,15 @@ class MainUIClass(QtGui.QMainWindow, Interface.Ui_MainWindow):
         self.botao_calculaEnergiaFormacao.clicked.connect(self.calcularEnergiaFormacao)
         self.botao_calculaEnergiaCoesao.setEnabled(False)
         self.botao_calculaEnergiaFormacao.setEnabled(False)
+        self.textBrowser.setVisible(False)
+        self.resultadoLabel.setVisible(False)
+        self.msg = QMessageBox()
+        self.msg.setIcon(QMessageBox.Warning)
         self.adicionarEntradas()
+
+    '''
+            Método responsável por adicionar as entryboxes na janela.
+    '''
 
     def adicionarEntradas(self):
         for i in range(30):
@@ -50,6 +57,11 @@ class MainUIClass(QtGui.QMainWindow, Interface.Ui_MainWindow):
 
             self.quantidadeEntradas += 1
 
+    '''
+            Método para salvar as entradas que foram escritas nas entryboxes duas listas, uma para atomos e outra para
+            a quantidade de atomos.
+    '''
+
     def leEntradas(self):
         for i in range(self.quantidadeEntradas):
             atomo = str(self.listaEntradasDeAtomos[i].text())
@@ -58,6 +70,10 @@ class MainUIClass(QtGui.QMainWindow, Interface.Ui_MainWindow):
                 if len(quantidade):
                     self.atomos.append(str(self.listaEntradasDeAtomos[i].text()).capitalize())
                     self.quantidades.append(int(self.listaEntradasNumeroDeAtomos[i].text()))
+
+    '''
+            Método para selecionar arquivo para a leitura
+    '''
 
     def selecionaArquivo(self):
         try:
@@ -82,79 +98,118 @@ class MainUIClass(QtGui.QMainWindow, Interface.Ui_MainWindow):
         self.botao_calculaEnergiaCoesao.setEnabled(True)
         self.botao_calculaEnergiaFormacao.setEnabled(True)
 
+    '''
+            Método para realizar o cálculo de energia de coesão
+    '''
 
     def calcularEnergiaCoesao(self):
-        try:
-            self.leEntradas()
-        except:
-            print("Erro na leitura de dados nas entryBOXes")
-            self.listaEntradasDeAtomos[:] = []
-            self.listaEntradasNumeroDeAtomos[:] = []
-            return
-        self.ec = True
-        self.ef = False
-        '''
-            Se o botão estiver ativado ele salva o método que sera utilizado
-            para fazer o calculo.
-
-        '''
         if self.metodoCalculo_A.isChecked():
             self.metodoEscolhido = 1
         if self.metodoCalculo_B.isChecked():
             self.metodoEscolhido = 2
         if self.metodoCalculo_C.isChecked():
             self.metodoEscolhido = 3
-        self.soma = Handler(self.atomos, self.quantidades, self.metodoEscolhido)
-        self.somatorioEnergiaAtomoLivre = self.soma.somatorioEATML()  # Resultado do somatorio de EATML
-        self.calc = Calculo(self.energia, self.somatorioEnergiaAtomoLivre, 0)
-        self.resultado = self.calc.calculo_energia_coesao()
-        self.geraArquivo()
-        self.geraGrafico()
-        return
+        if self.metodoEscolhido == -1:
+            self.msg.setText("AVISO")
+            self.msg.setInformativeText("Escolha um método para a realização do cálculo!")
+            self.msg.show()
+        else:
+            try:
+                self.leEntradas()
+            except:
+                print("Erro na leitura de dados nas entryBOXes")
+                self.listaEntradasDeAtomos[:] = []
+                self.listaEntradasNumeroDeAtomos[:] = []
+                return
+            self.ec = True
+            self.ef = False
+            '''
+                Se o botão estiver ativado ele salva o método que sera utilizado
+                para fazer o calculo.
+
+            '''
+            self.soma = Handler(self.atomos, self.quantidades, self.metodoEscolhido)
+            self.somatorioEnergiaAtomoLivre = self.soma.somatorioEATML()  # Resultado do somatorio de EATML
+            self.calc = Calculo(self.energia, self.somatorioEnergiaAtomoLivre, 0)
+            self.resultado = self.calc.calculo_energia_coesao()
+            self.geraArquivo()
+
+
+    '''
+            Método para realizar o cálculo de energia de formação
+    '''
 
     def calcularEnergiaFormacao(self):  # Método que da inicio ao cálculo
-        try:
-            self.leEntradas()
-        except:
-            print("Erro na leitura de dados nas entryBOXes")
+        if self.metodoCalculo_A.isChecked():
+            self.metodoEscolhido = 1
+        if self.metodoCalculo_B.isChecked():
+            self.metodoEscolhido = 2
+        if self.metodoCalculo_C.isChecked():
+            self.metodoEscolhido = 3
+        if self.metodoEscolhido == -1:
+            self.msg.setText("AVISO")
+            self.msg.setInformativeText("Escolha um método para a realização do cálculo!")
+            self.msg.show()
+        else:
+            try:
+                self.leEntradas()
+            except:
+                print("Erro na leitura de dados nas entryBOXes")
+                self.listaEntradasDeAtomos[:] = []
+                self.listaEntradasNumeroDeAtomos[:] = []
+                return
+            self.ef = True
+            self.ec = False
+            self.soma = Handler(self.atomos, self.quantidades, self.metodoEscolhido)
+            self.somatorioEnergiaCristalina = self.soma.somatorioECRYS()  # Resultado do somatorio de ECRYS
+            self.calc = Calculo(self.energia, 0, self.somatorioEnergiaCristalina)
+            self.resultado = self.calc.calculo_energia_formacao()
             self.listaEntradasDeAtomos[:] = []
             self.listaEntradasNumeroDeAtomos[:] = []
-            return
-        self.ef = True
-        self.ec = False
-        self.soma = Handler(self.atomos, self.quantidades)
-        self.somatorioEnergiaCristalina = self.soma.somatorioECRYS()  # Resultado do somatorio de ECRYS
-        self.calc = Calculo(self.energia, 0, self.somatorioEnergiaCristalina)
-        self.resultado = self.calc.calculo_energia_formacao()
-        print(self.resultado)
-        self.listaEntradasDeAtomos[:] = []
-        self.listaEntradasNumeroDeAtomos[:] = []
-        self.geraArquivo()
-        self.geraGrafico()
-        return
+            self.geraArquivo()
 
 
-    def geraArquivo(self):  # Método para gerar arquivo com os resultados do cálculo
+    '''
+            Método para gerar arquivo de resultado e mostrar no textBrowser os mesmos.
+    '''
+
+    def geraArquivo(self):
         try:
             self.arquivoSaida = open(self.arquivoLeitura + "__Resultados" + ".txt", "w")
             if len(self.volume) > 0 and self.ef:
                 self.arquivoSaida.write("VOLUME" + "         " + "ENERGIA" + "           " +
-                                   "ENERGIA DE FORMACAO\n\n")
+                                        "ENERGIA DE FORMACAO\n\n")
+                self.textBrowser.append("VOLUME" + "         " + "ENERGIA" + "           " +
+                                        "ENERGIA DE FORMACAO\n\n")
                 for i in range(len(self.resultado)):
                     self.arquivoSaida.write(str(self.volume[i]) + "       " + str(self.energia[i]) + "        " +
-                                       str(self.resultado[i]) + "\n")
+                                            str(self.resultado[i]) + "\n")
+                    self.textBrowser.append(str(self.volume[i]) + "       " + str(self.energia[i]) + "        " +
+                                            str(self.resultado[i]) + "\n")
                 self.arquivoSaida.close()
+                self.geraGrafico()
             elif self.ec:
                 self.arquivoSaida.write("VOLUME" + "         " + "ENERGIA" + "           " +
-                                   "ENERGIA DE Coesão\n\n")
+                                        "ENERGIA DE Coesão\n\n")
+                self.textBrowser.append("VOLUME" + "         " + "ENERGIA" + "           " +
+                                        "ENERGIA DE Coesão\n\n")
                 for i in range(len(self.resultado)):
                     self.arquivoSaida.write(str(self.volume[i]) + "       " + str(self.energia[i]) + "        " +
-                                       str(self.resultado[i]) + "\n")
+                                            str(self.resultado[i]) + "\n")
+                    self.textBrowser.append(str(self.volume[i]) + "       " + str(self.energia[i]) + "        " +
+                                            str(self.resultado[i]) + "\n")
                 self.arquivoSaida.close()
+                self.geraGrafico()
         except:
             print("erro ao criar arquivo")
             return
+        self.textBrowser.setVisible(True)
+        self.resultadoLabel.setVisible(True)
+        self.resetaEntradas()
 
+    '''
+            Método para gerar os gráficos com os resultados do cálculo.
+    '''
 
     def geraGrafico(self):
         if self.ec:
@@ -164,6 +219,7 @@ class MainUIClass(QtGui.QMainWindow, Interface.Ui_MainWindow):
                 plt.xlabel("Volume")
                 plt.scatter(self.volume, self.resultado, color='r')
                 plt.show()
+                self.resetaEntradas()
                 return
 
         if self.ef:
@@ -175,22 +231,39 @@ class MainUIClass(QtGui.QMainWindow, Interface.Ui_MainWindow):
                 plt.show()
                 return
 
-
-
     '''
             Método para reiniciar as entryboxes e as listas de atomos e numero deles.
     '''
+
     def resetaEntradas(self):
-        self.listaEntradasDeAtomos[:] = []
-        self.listaEntradasNumeroDeAtomos[:] = []
+        self.atomos[:] = []
+        self.quantidades[:] = []
+        for i in range(len(self.listaEntradasDeAtomos)):
+            self.listaEntradasDeAtomos[i].clear()
+            self.listaEntradasNumeroDeAtomos[i].clear()
         return
-
-
 
 
 if __name__ == '__main__':
     a = QtGui.QApplication(sys.argv)
     app = MainUIClass()
-    app.setFixedSize(958, 926)
+    '''
+
+        Seta a imagem de fundo da janela e o tamanho da mesma.
+
+    '''
+    icon = QtGui.QIcon()
+    icon.addPixmap(QtGui.QPixmap(_fromUtf8("resources/atom.jpg")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+    app.setWindowIcon(icon)
+    app.setWindowTitle("CeF 1.7")
+    palette = QPalette()
+    palette.setBrush(QPalette.Background, QBrush(QPixmap("resources/bg.png")))
+    app.setPalette(palette)
+    app.setFixedSize(870, 926)
+    '''
+
+        Mostra a janela na tela
+
+    '''
     app.show()
     a.exec_()
