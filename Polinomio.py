71f84869e7c7d8ce08512a04d22d9f9c17f37a81
Rafahel <rafahelmello@gmail.com>
2017-05-14 21:50:39 -0300

    Adicionado geração de formula

--- Polinomio.py
+++ Polinomio.py
@@ -1,12 +1,13 @@
 from numpy import *
-from scipy.interpolate import  *
+from scipy.interpolate import *
 
 
 def geradorPolinomio(x, y):
-    polinomio = polyfit(x, y, 2)
+    polinomio = polyfit(x, y, 3)
     xA = float(polinomio[0])
     xB = float(polinomio[1])
     xC = float(polinomio[2])
-    formula = "f(x) = (" + str(xA) + " * x1) + (" + str(xB) + " * x2) + (" + str(xC) + ")"
-    # print(formula)
+    xD = float(polinomio[3])
+    formula = "f(x) = (" + str(xA) + " * x3) + (" + str(xB) + " * x2) + (" + str(xC) + " * x) + (" + str(xD) + ")"
+    print(xD)
     return formula
